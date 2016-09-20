e6476bfa046e86f03077754066545ffb46863634
sithkazar <sithkazar@yahoo.com>
2016-09-20 12:43:53 -0500

    add

---
+++ holiday_files/vlb_engine/vlbdata6.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({borderSize:57,classNames:'vlightbox6',closeLocation:'top',descSliding:false,enableRightClick:false,enableSlideshow:false,prefix:'vlb6',resizeSpeed:7,slideTime:4,startZoom:true}) });
---
+++ maid_school_girl_files/vlb_engine/vlbdata7.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({borderSize:57,classNames:'vlightbox7',closeLocation:'top',descSliding:false,enableRightClick:false,enableSlideshow:false,prefix:'vlb7',resizeSpeed:7,slideTime:4,startZoom:true}) });

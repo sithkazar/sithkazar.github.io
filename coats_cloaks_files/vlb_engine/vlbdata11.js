38c7836bc4bd2071fbcd758888a20a7e844bca52
sithkazar <sithkazar@yahoo.com>
2019-09-27 11:07:12 -0500

    ff

---
+++ coats_cloaks_files/vlb_engine/vlbdata11.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({borderSize:57,classNames:'vlightbox11',closeLocation:'top',descSliding:false,enableRightClick:false,enableSlideshow:false,prefix:'vlb11',resizeSpeed:7,slideTime:4,startZoom:true}) });

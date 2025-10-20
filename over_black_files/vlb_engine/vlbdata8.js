24077dc2ae20ee9e5723cb0ca91e31b2a1281573
sithkazar <sithkazar@yahoo.com>
2025-10-20 14:39:22 -0500

    11

---
+++ over_black_files/vlb_engine/vlbdata8.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({borderSize:57,classNames:'vlightbox8',closeLocation:'top',descSliding:false,enableRightClick:false,enableSlideshow:false,prefix:'vlb8',resizeSpeed:7,slideTime:4,startZoom:true}) });
